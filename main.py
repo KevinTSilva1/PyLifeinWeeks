c39ded78f941080fe38bf4d5f37d13cc8fd66782
kevin.silva <kevin.silva@postalis.com.br>
2023-05-28 21:40:18 -0300

    feature: create the logic and function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-userAge = int(print("What is your current age? "))
+userAge = int(input("What is your current age? "))
+
+years_reaming = 90 - userAge
+days_reaming = years_reaming * 365
+months_reaming = years_reaming * 12
+weeks_reaming = years_reaming * 52
+
+print(f"You have {days_reaming} days, {weeks_reaming} weeks, {months_reaming} months left.")
